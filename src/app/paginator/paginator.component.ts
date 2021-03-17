fdeeeff1069d7cc0e86e5e34f34447cf2fd4b77e
Antoine-Flo <antoine.flouzat@gmail.com>
2021-03-17 17:53:36 +0100

    init

---
+++ src/app/paginator/paginator.component.ts
@@ -0,0 +1,69 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-paginator',
+  templateUrl: './paginator.component.html',
+  styleUrls: ['./paginator.component.scss']
+})
+export class PaginatorComponent {
+
+  currentPage = 0;
+
+  checkWindowIndex(i: number) {
+    if(i - this.currentPage < 4 && i - this.currentPage >= 0) return true
+    if(this.images.length - i < 5 && this.images.length - this.currentPage < 4) return true
+    return false
+  }
+
+  images = [
+    {
+      title: 'Some photo',
+      url: 'https://picsum.photos/id/1003/500',
+    },
+    {
+      title: 'Another one',
+      url: 'https://picsum.photos/id/1002/500',
+    },
+    {
+      title: 'Beautiful',
+      url: 'https://picsum.photos/id/10/500',
+    },
+    {
+      title: 'I like that',
+      url: 'https://picsum.photos/id/1000/500',
+    },
+    {
+      title: 'Some photo',
+      url: 'https://picsum.photos/id/1003/500',
+    },
+    {
+      title: 'Another one',
+      url: 'https://picsum.photos/id/1002/500',
+    },
+    {
+      title: 'Beautiful',
+      url: 'https://picsum.photos/id/10/500',
+    },
+    {
+      title: 'I like that',
+      url: 'https://picsum.photos/id/1000/500',
+    },
+    {
+      title: 'Some photo',
+      url: 'https://picsum.photos/id/1003/500',
+    },
+    {
+      title: 'Another one',
+      url: 'https://picsum.photos/id/1002/500',
+    },
+    {
+      title: 'Beautiful',
+      url: 'https://picsum.photos/id/10/500',
+    },
+    {
+      title: 'I like that',
+      url: 'https://picsum.photos/id/1000/500',
+    },
+  ];
+
+}
