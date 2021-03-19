0022160740da89e97b744094c680b45f841971ce
Antoine-Flo <antoine.flouzat@gmail.com>
2021-03-19 18:46:53 +0100

    delete code component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PaginatorComponent } from './paginator/paginator.component';
-import { CodeComponent } from './code/code.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    PaginatorComponent,
-    CodeComponent
+    PaginatorComponent
   ],
   imports: [
     BrowserModule,
